fa0f8dcabb8048b4fb4c3f3a7b0e4fe34f28007d
Henrique Chaltein <hxs1151@rit.edu>
2015-10-13 13:10:31 -0400

    Initial Commit. Started AI and projectile prefab implementation. Created BlackBoard AI class

---
+++ BossFight/Assets/Scripts/AICtrl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AICtrl : MonoBehaviour
+{
+    BlackBoard CurBBoard;
+    BlackBoard NextBBoard;
+
+    float BossHP = 100.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Get initial information from Specialists
+        
+        // Instantiate the BBoard
+
+        // 
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    // This is called once per frame, after the Update function of every object
+    void LateUpdate()
+    {
+        UpdateBlackBoard();
+    }
+
+    // Updates BlackBoard for next Frame
+    void UpdateBlackBoard()
+    {
+        CurBBoard = NextBBoard;
+    }
+}
---
+++ BossFight/Assets/Scripts/BlackBoard.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// blackBoard Object that contains all information on the scene
+public class BlackBoard
+{
+    // Transforms
+    Transform BossTransform;
+    Transform PlyrTransform;
+
+    // Health
+    float PlyrHP;
+    float BossHP;
+
+    // Distance to points of interest
+    Vector3 DistToLeftWall;
+    Vector3 DistToRighttWall;
+
+
+    // Method Calls
+    // constructor
+    public BlackBoard(Transform _BossTransform, Transform _PlyrTransform, 
+                        float _PlyrHP, float _BossHP,
+                        Vector3 _DistToLeftWall, Vector3 _DistToRighttWall)
+    {
+        // Transforms
+        BossTransform = _BossTransform;
+        PlyrTransform = _PlyrTransform;
+
+        // Health
+        PlyrHP = _PlyrHP;
+        BossHP = _BossHP;
+
+        // Distance to points of interest
+        DistToLeftWall = _DistToLeftWall;
+        DistToRighttWall = _DistToRighttWall;
+    }
+}
